b5c6b266e0ba877bc22bb0f85bb85846668df474
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-09-22 21:00:29 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { nanoid } from 'nanoid';
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import PhoneBookForm from './PhoneBookForm/PhoneBookForm';
 import ContactsList from './ContactsList/ContactsList';
 import Section from './Section/Section';
@@ -28,7 +28,7 @@ export class App extends Component {
       console.log(error);
     }
   }
-  componentWillUnmount() {}
+
   componentDidUpdate(prevProps, prevState, snapshot) {
     if (prevState !== this.state) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
@@ -68,7 +68,7 @@ export class App extends Component {
   render() {
     return (
       <div>
-        <Section title="PhoneBook">
+        <Section title="PhoneBook -> Class">
           <PhoneBookForm onInputContact={this.onInputContact} />
         </Section>
 
@@ -88,3 +88,95 @@ export class App extends Component {
     );
   }
 }
+
+export const App1 = () => {
+  const [filter, setFilter] = useState('');
+  // const [contacts, setContacts] = useState([
+  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  // ]);
+
+  // useEffect(() => {
+  //   console.log('DID__UPDATE=>>>>', contacts);
+  //   localStorage.setItem('contacts1', JSON.stringify(contacts));
+  // }, [contacts]);
+
+  const [contacts, setContacts] = useState(() => {
+    const contacts = localStorage.getItem('contacts1');
+    try {
+      const parsedContacts = JSON.parse(contacts);
+
+      if (parsedContacts) {
+        console.log('DID_MOUNT=>>>>', contacts);
+        return parsedContacts;
+      }
+      return [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ];
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  useEffect(() => {
+    const contacts = localStorage.getItem('contacts1');
+    try {
+      const parcedConacts = JSON.parse(contacts);
+
+      if (parcedConacts) {
+        console.log('DID_MOUNT=>>>>', contacts);
+        setContacts(parcedConacts);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const onInputContact = user => {
+    if (contacts.some(contact => contact.name === user.name)) {
+      return alert(`${user.name} is already in contacts.`);
+    }
+    user.id = nanoid();
+    setContacts(prevState => [...prevState, user]);
+  };
+
+  const findByName = value => {
+    const name = value.trim().toLocaleLowerCase();
+    if (!name) {
+      setFilter(name);
+      return;
+    }
+    const res = contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(name)
+    );
+    console.log(res);
+    setFilter(res);
+  };
+
+  const onClickDelete = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
+  };
+  return (
+    <div>
+      <Section title="PhoneBook -> Hook">
+        <PhoneBookForm onInputContact={onInputContact} />
+      </Section>
+
+      <Section title="Contacts">
+        <InputSearch
+          nameSearch="Find contacts by name"
+          onSearchName={findByName}
+        />
+        <ContactsList
+          onClickDelete={onClickDelete}
+          contacts={filter === '' ? contacts : filter}
+        />
+      </Section>
+    </div>
+  );
+};
